3afae2169620ed8937adadef15dbf3cf327104b8
Olga <olga.itgirl@gmail.com>
2022-08-15 16:30:21 +0500

    update calculator

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// theme 
 
 const html = document.querySelector('html');
 const check = document.getElementById('switch');
@@ -8,5 +9,92 @@ check.addEventListener("change", function(){
   } else{
     html.setAttribute('data-theme', 'dark');
   }
-})
+});
+
+
+// calculator 
+
+let a = ''; // first num
+let b = ''; // second num
+let sign = ''; // operation sign
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['+', '-', '×', '÷'];
+
+const result = document.getElementById('result');
+
+function clearAll (){
+  a = '';
+  b = '';
+  sign = '';
+  finish = false;
+  result.textContent = 0;
+}
+
+document.querySelector('.btn_clear').onclick = clearAll;
+
+document.querySelector('.calc__buttons').onclick = (event) => {
+  
+  if (!event.target.classList.contains('btn')) return;
+  if (event.target.classList.contains('btn_clear')) return;
+
+  result.textContent = '';
+  const key = event.target.textContent;
+  
+  if (digit.includes(key)) {
+    if (b === '' && sign === '') {
+      a += key;
+      result.textContent = a;
+    }
+    else if (a !== '' && b !== '' && finish) {
+      b = key;
+      finish = false;
+      result.textContent = b;
+
+    }
+    else {
+      b += key;
+      result.textContent = b;
+    }
+    return;
+  }
+
+  if (action.includes(key)) {
+    sign = key;
+    result.textContent = sign;
+  }
+
+  if (key === '=') {
+
+    if(b === '') b = a;
+
+    switch (sign) {
+      case "+":
+        a = (+a) + (+b);
+        break;
+      case "-":
+        a= a - b;
+        break;
+      case "×":
+        a = a * b;
+        break;
+      case "÷":
+        if(b === '0') {
+          result.textContent = 'error';
+          a = '';
+          b = '';
+          sign = '';
+          return;
+        };
+        a = a / b;
+        break;
+    }
+
+    finish = true;
+    result.textContent = a;
+  }
+
+} ;
+
 
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 # Responsive calculator with dark/light theme 
 
+- dark/light theme; 
+- responsive;
+...in progress 
+
 #### [see this calculator](https://oljs.github.io/calculator/)
 
-- dark/light theme; 
-- responsive font;
-
-...in progress
